f7490d4c0bdd3804f59cf9811101781b367189e9
V01D19 <khaleded75215@gmail.com>
2026-04-15 17:29:14 +0200

    Setup Project Strucure , Folders

---
+++ GymMaster/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GymMaster.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string PlanType { get; set; } // شهر، 3 شهور، سنة
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ GymMaster/Utilities/DataStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GymMaster.Models;
+using System.Collections.Generic;
+
+namespace GymMaster.Utilities
+{
+    public static class DataStorage
+    {
+        // القائمة دي هي الداتابيز
+        public static List<Member> AllMembers = new List<Member>();
+    }
+}
