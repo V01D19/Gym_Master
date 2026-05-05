db8a5cde6bd365dbb7cb47c7b86d009644fa617d
V01D19 <khaleded75215@gmail.com>
2026-05-05 10:03:32 +0300

    before discussion

---
+++ GymMaster/Views/Logic_Form.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace GymMaster.Views
+{
+    partial class Logic_Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtEndDate = new System.Windows.Forms.TextBox();
+            this.dtStartDate = new System.Windows.Forms.DateTimePicker();
+            this.lblMainTitle = new System.Windows.Forms.Label();
+            this.lblStartTag = new System.Windows.Forms.Label();
+            this.lblEndTag = new System.Windows.Forms.Label();
+            this.lblSelectType = new System.Windows.Forms.Label();
+            this.btnMonth = new System.Windows.Forms.Button();
+            this.btn3Months = new System.Windows.Forms.Button();
+            this.btn6Months = new System.Windows.Forms.Button();
+            this.btnYear = new System.Windows.Forms.Button();
+            this.btnSubscribe = new System.Windows.Forms.Button();
+            this.UserName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.Number = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+
+            // txtEndDate
+            this.txtEndDate.BackColor = System.Drawing.Color.Black;
+            this.txtEndDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEndDate.ForeColor = System.Drawing.Color.Lime;
+            this.txtEndDate.Location = new System.Drawing.Point(627, 194);
+            this.txtEndDate.Name = "txtEndDate";
+            this.txtEndDate.ReadOnly = true;
+            this.txtEndDate.Size = new System.Drawing.Size(139, 23);
+            this.txtEndDate.TabIndex = 9;
+            this.txtEndDate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+
+            // dtStartDate
+            this.dtStartDate.Location = new System.Drawing.Point(627, 156);
+            this.dtStartDate.Name = "dtStartDate";
+            this.dtStartDate.Size = new System.Drawing.Size(139, 23);
+            this.dtStartDate.TabIndex = 14;
+
+            // lblMainTitle
+            this.lblMainTitle.AutoSize = true;
+            this.lblMainTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblMainTitle.ForeColor = System.Drawing.Color.Goldenrod;
+            this.lblMainTitle.Location = new System.Drawing.Point(251, 84);
+            this.lblMainTitle.Name = "lblMainTitle";
+            this.lblMainTitle.Text = "GymMaster Subscription Management";
+
+            // lblStartTag
+            this.lblStartTag.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblStartTag.ForeColor = System.Drawing.Color.White;
+            this.lblStartTag.Location = new System.Drawing.Point(468, 157);
+            this.lblStartTag.Text = "Start Date";
+
+            // lblEndTag
+            this.lblEndTag.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblEndTag.ForeColor = System.Drawing.Color.White;
+            this.lblEndTag.Location = new System.Drawing.Point(468, 194);
+            this.lblEndTag.Text = "End Date";
+
+            // lblSelectType
+            this.lblSelectType.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblSelectType.ForeColor = System.Drawing.Color.White;
+            this.lblSelectType.Location = new System.Drawing.Point(345, 279);
+            this.lblSelectType.Size = new System.Drawing.Size(278, 37);
+            this.lblSelectType.Text = "Select Subscription Type:";
+
+            // btnMonth
+            this.btnMonth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMonth.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnMonth.ForeColor = System.Drawing.Color.White;
+            this.btnMonth.Location = new System.Drawing.Point(317, 332);
+            this.btnMonth.Size = new System.Drawing.Size(113, 33);
+            this.btnMonth.Text = "1 Month";
+            this.btnMonth.Click += new System.EventHandler(this.btnMonth_Click);
+
+            // btn3Months
+            this.btn3Months.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3Months.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btn3Months.ForeColor = System.Drawing.Color.White;
+            this.btn3Months.Location = new System.Drawing.Point(468, 332);
+            this.btn3Months.Size = new System.Drawing.Size(113, 33);
+            this.btn3Months.Text = "3 Months";
+            this.btn3Months.Click += new System.EventHandler(this.btn3Month_Click);
+
+            // btn6Months
+            this.btn6Months.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6Months.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btn6Months.ForeColor = System.Drawing.Color.White;
+            this.btn6Months.Location = new System.Drawing.Point(317, 393);
+            this.btn6Months.Size = new System.Drawing.Size(113, 33);
+            this.btn6Months.Text = "6 Months";
+            this.btn6Months.Click += new System.EventHandler(this.btn6Month_Click);
+
+            // btnYear
+            this.btnYear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYear.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnYear.ForeColor = System.Drawing.Color.White;
+            this.btnYear.Location = new System.Drawing.Point(468, 393);
+            this.btnYear.Size = new System.Drawing.Size(113, 33);
+            this.btnYear.Text = "Full Year";
+            this.btnYear.Click += new System.EventHandler(this.btnYear_Click);
+
+            // btnSubscribe
+            this.btnSubscribe.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnSubscribe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubscribe.ForeColor = System.Drawing.Color.Black;
+            this.btnSubscribe.Location = new System.Drawing.Point(369, 475);
+            this.btnSubscribe.Size = new System.Drawing.Size(150, 40);
+            this.btnSubscribe.Text = "Confirm Subscription";
+
+            // UserName
+            this.UserName.AutoSize = true;
+            this.UserName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.UserName.ForeColor = System.Drawing.Color.White;
+            this.UserName.Location = new System.Drawing.Point(103, 156);
+            this.UserName.Text = "Name";
+
+            // txtName
+            this.txtName.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.ForeColor = System.Drawing.Color.White;
+            this.txtName.Location = new System.Drawing.Point(251, 156);
+            this.txtName.Size = new System.Drawing.Size(142, 23);
+            this.txtName.TabIndex = 0;
+
+            // Number
+            this.Number.AutoSize = true;
+            this.Number.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.Number.ForeColor = System.Drawing.Color.White;
+            this.Number.Location = new System.Drawing.Point(103, 199);
+            this.Number.Text = "Phone Number";
+
+            // txtPhone
+            this.txtPhone.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPhone.ForeColor = System.Drawing.Color.White;
+            this.txtPhone.Location = new System.Drawing.Point(251, 197);
+            this.txtPhone.Size = new System.Drawing.Size(142, 23);
+            this.txtPhone.TabIndex = 1;
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(103, 238);
+            this.label1.Text = "Price";
+
+            // txtPrice
+            this.txtPrice.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.txtPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPrice.ForeColor = System.Drawing.Color.White;
+            this.txtPrice.Location = new System.Drawing.Point(251, 238);
+            this.txtPrice.ReadOnly = true;
+            this.txtPrice.Size = new System.Drawing.Size(142, 23);
+            this.txtPrice.TabIndex = 3;
+
+            // Logic_Form
+            this.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.ClientSize = new System.Drawing.Size(884, 581);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.UserName);
+            this.Controls.Add(this.btnSubscribe);
+            this.Controls.Add(this.lblMainTitle);
+            this.Controls.Add(this.lblStartTag);
+            this.Controls.Add(this.dtStartDate);
+            this.Controls.Add(this.btnMonth);
+            this.Controls.Add(this.btn3Months);
+            this.Controls.Add(this.btn6Months);
+            this.Controls.Add(this.btnYear);
+            this.Controls.Add(this.lblEndTag);
+            this.Controls.Add(this.txtEndDate);
+            this.Controls.Add(this.lblSelectType);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(900, 620);
+            this.MinimumSize = new System.Drawing.Size(900, 620);
+            this.Name = "Logic_Form";
+            this.Load += new System.EventHandler(this.Logic_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEndDate;
+        private System.Windows.Forms.DateTimePicker dtStartDate;
+        private System.Windows.Forms.Label lblMainTitle;
+        private System.Windows.Forms.Label lblStartTag;
+        private System.Windows.Forms.Label lblEndTag;
+        private System.Windows.Forms.Label lblSelectType;
+        private System.Windows.Forms.Button btnMonth;
+        private System.Windows.Forms.Button btn3Months;
+        private System.Windows.Forms.Button btn6Months;
+        private System.Windows.Forms.Button btnYear;
+        private System.Windows.Forms.Label UserName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label Number;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnSubscribe;
+    }
+}
--- GymMaster/Views/Logic_Form.cs
+++ GymMaster/Views/Logic_Form.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 using GymMaster.Models;
 
@@ -8,18 +9,8 @@ namespace GymMaster.Views
 {
     public partial class Logic_Form : Form
     {
-        private TextBox txtEndDate;
-        private DateTimePicker dtStartDate;
-        private Label lblMainTitle, lblStartTag, lblEndTag, lblSelectType;
-        private Button btnMonth, btn3Months, btn6Months, btnYear;
-        private Label UserName;
-        private TextBox txtName;
-        private Label Number;
-        private TextBox txtPhone;
-        private Label label1;
-        private TextBox txtPrice;
-        private string selectedSubscriptionType;
-        private Button btnSubscribe;
+        //  enum 
+        private SubscriptionPlan selectedSubscriptionType;
 
         public Logic_Form()
         {
@@ -28,203 +19,57 @@ namespace GymMaster.Views
 
             InitializeComponent();
 
-            btnSubscribe.Click += btnSubscribe_Click;
-
-            txtName.KeyPress += (s, e) => { if (char.IsDigit(e.KeyChar)) e.Handled = true; };
+       
+            txtName.KeyPress += new KeyPressEventHandler(txtName_KeyPress);
             txtPhone.MaxLength = 11;
-            txtPhone.KeyPress += (s, e) =>
+            txtPhone.KeyPress += new KeyPressEventHandler(txtPhone_KeyPress);
+            txtName.KeyDown += new KeyEventHandler(txtName_KeyDown);
+        }
+
+
+        private void SetSubscription(int months, SubscriptionPlan planType, string price)
+        {
+            DateTime endDate;
+
+            if (months == 12)
             {
-                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true;
-            };
-
-            txtName.KeyDown += MoveToNextControl;
-        }
-
-        private void InitializeComponent()
-        {
-            txtEndDate = new TextBox();
-            dtStartDate = new DateTimePicker();
-            lblMainTitle = new Label();
-            lblStartTag = new Label();
-            lblEndTag = new Label();
-            lblSelectType = new Label();
-            btnMonth = new Button();
-            btn3Months = new Button();
-            btn6Months = new Button();
-            btnYear = new Button();
-            btnSubscribe = new Button();
-            UserName = new Label();
-            txtName = new TextBox();
-            Number = new Label();
-            txtPhone = new TextBox();
-            label1 = new Label();
-            txtPrice = new TextBox();
-            SuspendLayout();
-
-            txtEndDate.BackColor = Color.Black;
-            txtEndDate.BorderStyle = BorderStyle.FixedSingle;
-            txtEndDate.ForeColor = Color.Lime;
-            txtEndDate.Location = new Point(627, 194);
-            txtEndDate.Name = "txtEndDate";
-            txtEndDate.ReadOnly = true;
-            txtEndDate.Size = new Size(139, 23);
-            txtEndDate.TabIndex = 9;
-            txtEndDate.TextAlign = HorizontalAlignment.Center;
-
-            dtStartDate.Location = new Point(627, 156);
-            dtStartDate.Name = "dtStartDate";
-            dtStartDate.Size = new Size(139, 23);
-            dtStartDate.TabIndex = 14;
-
-            lblMainTitle.AutoSize = true;
-            lblMainTitle.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
-            lblMainTitle.ForeColor = Color.Goldenrod;
-            lblMainTitle.Location = new Point(251, 84);
-            lblMainTitle.Name = "lblMainTitle";
-            lblMainTitle.Text = "GymMaster Subscription Management";
-
-            lblStartTag.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            lblStartTag.ForeColor = Color.White;
-            lblStartTag.Location = new Point(468, 157);
-            lblStartTag.Text = "Start Date";
-
-            lblEndTag.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            lblEndTag.ForeColor = Color.White;
-            lblEndTag.Location = new Point(468, 194);
-            lblEndTag.Text = "End Date";
-
-            lblSelectType.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            lblSelectType.ForeColor = Color.White;
-            lblSelectType.Location = new Point(345, 279);
-            lblSelectType.Size = new Size(278, 37);
-            lblSelectType.Text = "Select Subscription Type:";
-
-            btnMonth.FlatStyle = FlatStyle.Flat;
-            btnMonth.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
-            btnMonth.ForeColor = Color.White;
-            btnMonth.Location = new Point(317, 332);
-            btnMonth.Size = new Size(113, 33);
-            btnMonth.Text = "1 Month";
-            btnMonth.Click += btnMonth_Click;
-
-            btn3Months.FlatStyle = FlatStyle.Flat;
-            btn3Months.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
-            btn3Months.ForeColor = Color.White;
-            btn3Months.Location = new Point(468, 332);
-            btn3Months.Size = new Size(113, 33);
-            btn3Months.Text = "3 Months";
-            btn3Months.Click += btn3Month_Click;
-
-            btn6Months.FlatStyle = FlatStyle.Flat;
-            btn6Months.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
-            btn6Months.ForeColor = Color.White;
-            btn6Months.Location = new Point(317, 393);
-            btn6Months.Size = new Size(113, 33);
-            btn6Months.Text = "6 Months";
-            btn6Months.Click += btn6Month_Click;
-
-            btnYear.FlatStyle = FlatStyle.Flat;
-            btnYear.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
-            btnYear.ForeColor = Color.White;
-            btnYear.Location = new Point(468, 393);
-            btnYear.Size = new Size(113, 33);
-            btnYear.Text = "Full Year";
-            btnYear.Click += btnYear_Click;
-
-            btnSubscribe.BackColor = Color.Goldenrod;
-            btnSubscribe.FlatStyle = FlatStyle.Flat;
-            btnSubscribe.ForeColor = Color.Black;
-            btnSubscribe.Location = new Point(369, 475);
-            btnSubscribe.Size = new Size(150, 40);
-            btnSubscribe.Text = "Confirm Subscription";
-
-            UserName.AutoSize = true;
-            UserName.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            UserName.ForeColor = Color.White;
-            UserName.Location = new Point(103, 156);
-            UserName.Text = "Name";
-
-            txtName.BackColor = Color.FromArgb(30, 30, 30);
-            txtName.BorderStyle = BorderStyle.FixedSingle;
-            txtName.ForeColor = Color.White;
-            txtName.Location = new Point(251, 156);
-            txtName.Size = new Size(142, 23);
-            txtName.TabIndex = 0;
-
-            Number.AutoSize = true;
-            Number.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            Number.ForeColor = Color.White;
-            Number.Location = new Point(103, 199);
-            Number.Text = "Phone Number";
-
-            txtPhone.BackColor = Color.FromArgb(30, 30, 30);
-            txtPhone.BorderStyle = BorderStyle.FixedSingle;
-            txtPhone.ForeColor = Color.White;
-            txtPhone.Location = new Point(251, 197);
-            txtPhone.Size = new Size(142, 23);
-            txtPhone.TabIndex = 1;
-
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(103, 238);
-            label1.Text = "Price";
-
-            txtPrice.BackColor = Color.FromArgb(30, 30, 30);
-            txtPrice.BorderStyle = BorderStyle.FixedSingle;
-            txtPrice.ForeColor = Color.White;
-            txtPrice.Location = new Point(251, 238);
-            txtPrice.ReadOnly = true;
-            txtPrice.Size = new Size(142, 23);
-            txtPrice.TabIndex = 3;
-
-            BackColor = Color.FromArgb(30, 30, 30);
-            ClientSize = new Size(884, 581);
-            Controls.Add(label1);
-            Controls.Add(txtPrice);
-            Controls.Add(txtPhone);
-            Controls.Add(txtName);
-            Controls.Add(Number);
-            Controls.Add(UserName);
-            Controls.Add(btnSubscribe);
-            Controls.Add(lblMainTitle);
-            Controls.Add(lblStartTag);
-            Controls.Add(dtStartDate);
-            Controls.Add(btnMonth);
-            Controls.Add(btn3Months);
-            Controls.Add(btn6Months);
-            Controls.Add(btnYear);
-            Controls.Add(lblEndTag);
-            Controls.Add(txtEndDate);
-            Controls.Add(lblSelectType);
-            Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            MaximizeBox = false;
-            MaximumSize = new Size(900, 620);
-            MinimumSize = new Size(900, 620);
-            Name = "Logic_Form";
-            Load += Logic_Form_Load;
-            ResumeLayout(false);
-            PerformLayout();
-        }
-
-        private void SetSubscription(int months, string planType, string price)
-        {
-            DateTime endDate = months == 12
-                ? dtStartDate.Value.AddYears(1)
-                : dtStartDate.Value.AddMonths(months);
+                endDate = dtStartDate.Value.AddYears(1);
+            }
+            else
+            {
+                endDate = dtStartDate.Value.AddMonths(months);
+            }
 
             txtEndDate.Text = endDate.ToShortDateString();
             txtPrice.Text = price;
             selectedSubscriptionType = planType;
         }
 
-        private void btnMonth_Click(object sender, EventArgs e) => SetSubscription(1, "1 Month", "500");
-        private void btn3Month_Click(object sender, EventArgs e) => SetSubscription(3, "3 Months", "1400");
-        private void btn6Month_Click(object sender, EventArgs e) => SetSubscription(6, "6 Months", "2500");
-        private void btnYear_Click(object sender, EventArgs e) => SetSubscription(12, "Full Year", "5000");
+      
+        private void btnMonth_Click(object sender, EventArgs e)
+        {
+            SetSubscription(1, SubscriptionPlan.OneMonth, "500");
+        }
 
+        private void btn3Month_Click(object sender, EventArgs e)
+        {
+            SetSubscription(3, SubscriptionPlan.ThreeMonths, "1400");
+        }
+
+        private void btn6Month_Click(object sender, EventArgs e)
+        {
+            SetSubscription(6, SubscriptionPlan.SixMonths, "2500");
+        }
+
+        private void btnYear_Click(object sender, EventArgs e)
+        {
+            SetSubscription(12, SubscriptionPlan.FullYear, "5000");
+        }
+
+      
         private void btnSubscribe_Click(object sender, EventArgs e)
         {
+           
             if (string.IsNullOrWhiteSpace(txtName.Text) ||
                 string.IsNullOrWhiteSpace(txtPhone.Text) ||
                 string.IsNullOrWhiteSpace(txtEndDate.Text))
@@ -243,7 +88,7 @@ namespace GymMaster.Views
 
             try
             {
-               
+                
                 Member newMember = new Member();
                 newMember.Name = txtName.Text;
                 newMember.Phone = txtPhone.Text;
@@ -252,16 +97,15 @@ namespace GymMaster.Views
                 newMember.EndDate = DateTime.Parse(txtEndDate.Text);
                 newMember.Price = decimal.Parse(txtPrice.Text);
 
-
                 File.AppendAllText("Subscribers.txt", newMember.GetRecordString() + Environment.NewLine);
 
                 MessageBox.Show("Member added successfully");
 
+             
                 txtName.Clear();
                 txtPhone.Clear();
                 txtEndDate.Clear();
                 txtPrice.Clear();
-                selectedSubscriptionType = null;
                 dtStartDate.Value = DateTime.Now;
             }
             catch (Exception ex)
@@ -270,14 +114,33 @@ namespace GymMaster.Views
             }
         }
 
-        private void Logic_Form_Load(object sender, EventArgs e) { }
+        private void Logic_Form_Load(object sender, EventArgs e)
+        {
+        }
 
-        private void MoveToNextControl(object sender, KeyEventArgs e)
+    
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtPhone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtName_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
                 e.SuppressKeyPress = true;
-                this.SelectNextControl((Control)sender, true, true, true, true);
+                txtPhone.Focus();
             }
         }
     }
